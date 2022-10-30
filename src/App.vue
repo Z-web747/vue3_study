b7dd16ec1da42ba5c4c580267005772cae6f4121
zhenghemin <zhenghemin@rr.tv>
2022-10-30 21:54:04 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
-  <PullRefreshome/>
+<div>
+  
+</div>
 </template>
 
 <script>
-import PullRefreshome from './PullRefresh.vue'
+import * as THREE from 'three';
 
+// 方式 1: 导入整个 three.js核心库
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 export default {
-  name: 'App',
-  components: {
-    PullRefreshome
+  setup() {
+    const loader = new GLTFLoader();
+    const scene = new THREE.Scene();
+
+    loader.load( '/static/Duck.gltf', function (  ) {}, undefined, function ( error ) {
+
+      console.error( error );
+
+    } );
+// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
   }
 }
 </script>
-
-<style>
-*{
-  padding:0;
-  margin: 0;
-}
-</style>
