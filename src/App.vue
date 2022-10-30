6ace358c81fa032052cbaa75494c9b86d2fa087a
zhenghemin <zhenghemin@rr.tv>
2022-10-30 21:54:59 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 export default {
   setup() {
+    console.log(123);
     const loader = new GLTFLoader();
     const scene = new THREE.Scene();
 
